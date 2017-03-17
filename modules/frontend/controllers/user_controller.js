45acbabc862b77b1a3fe6e38c11093c811a70ad5
jitendra <jitendra_kumar@seologistics.com>
2017-03-17 18:26:22 +0530

    make it better

--- modules/frontend/controllers/user_controller.js
+++ modules/frontend/controllers/user_controller.js
@@ -1,16 +1,4 @@
 const fs		=	require('fs'),
 	path		=	require('path');
 
-	
 
-
-
-
-fs.watch('hello1/one.js', (eventType, filename) => {
-  console.log(`event type is: ${eventType}`);
-  if (filename) {
-    console.log(`filename provided: ${filename}`);
-  } else {
-    console.log('filename not provided');
-  }
-});	
